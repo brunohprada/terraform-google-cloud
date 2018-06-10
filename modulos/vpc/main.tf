76238fab57aba56159f8bfa4e1e63a60209a8fed
brunohprada <brunohprada@hotmail.com>
2018-06-10 10:42:08 -0300

    Correção do link network no recurso google_compute_subnetwork.

--- modulos/vpc/main.tf
+++ modulos/vpc/main.tf
@@ -5,7 +5,7 @@ resource "google_compute_network" "default" {
 resource "google_compute_subnetwork" "default" {
   name = "${var.prefix_name}-${var.network_name}"
   ip_cidr_range = "${var.network_cidr}"
-  network = "${google_compute_subnetwork.default.self_link}"
+  network = "${google_compute_network.default.self_link}"
   region = "${var.region}"
   private_ip_google_access = true
 }
