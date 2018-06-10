458f952fcf41a345c51ca051846733b506db8747
brunohprada <brunohprada@hotmail.com>
2018-06-10 11:13:11 -0300

    Correções

--- modulos/vpc/main.tf
+++ modulos/vpc/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "default" {
   auto_create_subnetworks = "false"
 }
 resource "google_compute_subnetwork" "default" {
-  name = "${var.prefix_name}-${var.subnetwork-name}"
+  name = "${var.prefix_name}-${var.subnetwork_name}"
   ip_cidr_range = "${var.network_cidr}"
   network = "${google_compute_network.default.self_link}"
   region = "${var.region}"
