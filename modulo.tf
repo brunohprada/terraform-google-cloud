b2e4da3f962262eee27ae3fa347923d03e187bda
brunohprada <brunohprada@hotmail.com>
2018-06-12 06:35:33 -0300

    test again

--- modulo.tf
+++ modulo.tf
@@ -18,5 +18,5 @@ module "instance" {
     machine_type = "n1-standard-1"
     zone = "southamerica-east1-a"
     network_instance = "projeto-subnetwork"
-    depends_on = ["module.vpc"]
+    depends_on = ["vpc"]
 }
